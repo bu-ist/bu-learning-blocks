a3c39aa66a0f6353411589f98d46fd4484051cfd
DannyCrews <dancrews@me.com>
2018-08-03 09:23:45 -0400

    Add text alignment toolbar.

--- src/bulb-tf/index.js
+++ src/bulb-tf/index.js
@@ -6,6 +6,7 @@
 
 // Import block dependencies and components
 import Inspector from './components/inspector';
+import classnames from 'classnames';
 
 //  Import CSS.
 import './styles/style.scss';
@@ -13,7 +14,11 @@ import './styles/editor.scss';
 
 // Components
 const { __ } = wp.i18n;
-const { RichText } = wp.editor;
+const {
+	RichText,
+	AlignmentToolbar,
+	BlockControls,
+} = wp.editor;
 
 // Extend component
 const { Component } = wp.element;
@@ -39,18 +44,30 @@ registerBlockType( 'bulb/question-tf',
 				source: 'children',
 				selector: '.question-body',
 			},
+			textAlignment: {
+				type: 'string',
+				default: 'left',
+			},
 		},
 
 		edit: props => {
-			const { attributes: { question }, className, setAttributes } = props;
+			const { attributes: { textAlignment, question },
+				className, setAttributes } = props;
 			const onChangeQuestion = question => { setAttributes( { question } ); };
 			return (
 				<div className={ className }>
 					<h2>{ __( 'Test Your Knowledge', 'bulearningblocks' ) }</h2>
+					<BlockControls>
+						<AlignmentToolbar
+							value={ textAlignment }
+							onChange={ textAlignment => setAttributes( { textAlignment } ) }
+						/>
+					</BlockControls>
 					<RichText
 						tagName="div"
 						multiline="p"
 						placeholder={ __( 'Write your question here!', 'bulearningblocks' ) }
+						style={ { textAlign: textAlignment } }
 						onChange={ onChangeQuestion }
 						value={ question }
 					/>
@@ -59,11 +76,14 @@ registerBlockType( 'bulb/question-tf',
 		},
 
 		save: props => {
-			const { attributes: { question } } = props;
+			const { textAlignment, message } = props.attributes;
 			return (
 				<div>
 					<h2>{ __( 'True/False Question', 'bulearningblocks' ) }</h2>
-					<div className="question-body">
+					<div
+						className="question-body"
+						style={ { textAlign: textAlignment } }
+					>
 						{ question }
 					</div>
 				</div>
