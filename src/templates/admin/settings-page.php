c5c7f12c9273aeeadbc00991949e31cccecb516c
DannyCrews <dancrews@me.com>
2018-08-15 16:44:06 -0400

    Fix translation function error.

--- src/templates/admin/settings-page.php
+++ src/templates/admin/settings-page.php
@@ -1,14 +1,22 @@
-<div class="wrap">
-	<h1>
-		<?php esc_html_e( get_admin_page_title() ); ?>
-	</h1>
+<?php
+/**
+ * BULB Plugin Templates
+ *
+ * Primarily HTML markup for the admin settings page.
+ *
+ * @package BU Learning Blocks
+ */
 
+?>
+
+<div class="wrap">
+	<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
 	<form method="post" action="options.php">
-	<!-- Display necessary hidden fields for settings -->
-	<?php settings_fields( 'bulb_settings' ); ?>
-	<!-- Display the settings sections for the page -->
-	<?php do_settings_sections( 'bulb' ); ?>
-	<!-- Default Submit Button -->
-	<?php submit_button(); ?>
+		<!-- Display necessary hidden fields for settings -->
+		<?php settings_fields( 'bulb_settings' ); ?>
+		<!-- Display the settings sections for the page -->
+		<?php do_settings_sections( 'bulb' ); ?>
+		<!-- Default Submit Button -->
+		<?php submit_button(); ?>
 	</form>
 </div>
