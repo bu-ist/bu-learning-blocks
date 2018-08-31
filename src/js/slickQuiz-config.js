fbc88387593068564767113cddde45ca80467100
DannyCrews <dancrews@me.com>
2018-08-31 15:40:10 -0400

    Fix a few bugs and rem0ve debug code.

--- src/js/slickQuiz-config.js
+++ src/js/slickQuiz-config.js
@@ -1,15 +1,14 @@
 // Setup your quiz text and questions here
 const questionId = document.querySelector( '.bulb-question' ).id;
 const question = window[ questionId ];
-alert( JSON.stringify( question, null, 4 ) );
 
 const quizJSON = {
 	info: {
-		main: '<p>' + question.header[ 0 ] + '</p>',
+		main: '<p>' + question.header[ 0 ].props.children + '</p>',
 	},
 	questions: [
 		{
-			q: question.body[ 0 ],
+			q: question.body[ 0 ].props.children,
 			a: [
 				{ option: 'True', correct: true },
 				{ option: 'False', correct: false },
