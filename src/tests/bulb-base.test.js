c134c498c596900835b8850b4bf5cd86128d8e49
Jonathan Williams <tamlin@mac.com>
2018-09-11 13:40:10 -0400

    basic enzyme setup

--- src/tests/bulb-base.test.js
+++ src/tests/bulb-base.test.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 
 test( 'should be true', () => {
 	expect( true ).toBe( true );
