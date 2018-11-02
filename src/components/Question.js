23b230eec802415a4bc2556dda163b2723825303
Carlos E Silva <cedas@bu.edu>
2018-11-02 14:23:59 -0400

    Create question container

---
+++ src/components/Question.js
@@ -0,0 +1,55 @@
+import classnames from 'classnames';
+
+import QuestionHeader from './QuestionHeader';
+import QuestionBody from './QuestionBody';
+import QuestionFeedback from './QuestionFeedback';
+
+export default ( {
+	classes,
+	header,
+	onChangeHeader,
+	body,
+	onChangeBody,
+	singleFeedback,
+	feedback,
+	onChangeFeedback,
+	correctFeedback,
+	onChangeCorrectFeedback,
+	incorrectFeedback,
+	onChangeIncorrectFeedback,
+	textAlignment,
+	textColorControl,
+	backgroundColorControl,
+	fontSize,
+	children,
+} ) => {
+	const styles = {
+		textAlignment,
+		textColorControl,
+		backgroundColorControl,
+		fontSize,
+	};
+	return (
+		<div className={ classnames( 'bulb-question', classes ) }>
+			<QuestionHeader value={ header } onChange={ onChangeHeader } { ...styles } />
+			<QuestionBody value={ body } onChange={ onChangeBody } { ...styles } />
+			{ children }
+			{ singleFeedback ? (
+				<QuestionFeedback
+					singleFeedback
+					feedback={ feedback }
+					onChangeFeedback={ onChangeFeedback }
+					{ ...styles }
+				/>
+			) : (
+				<QuestionFeedback
+					correctFeedback={ correctFeedback }
+					onCorrectFeedbackChange={ onChangeCorrectFeedback }
+					incorrectFeedback={ incorrectFeedback }
+					onIncorrectFeedbackChange={ onChangeIncorrectFeedback }
+					{ ...styles }
+				/>
+			) }
+		</div>
+	);
+};
