ee3838b238f5a47586f7a24610c3754be4801dc8
Carlos E Silva <cedas@bu.edu>
2018-12-12 12:20:56 -0500

    Render frontend with the react lib

--- src/frontend.js
+++ src/frontend.js
@@ -3,5 +3,14 @@
  *
  * Handles frontend logic...
  */
+import ReactDOM from 'react-dom';
 
-alert( 'frontend bundle run successfully' );
+// Import the react-questions library.
+import { Question } from 'react-questions';
+
+// Find all .bulb-question DOM containers, collect their data from window and render <Questions> into them.
+document.querySelectorAll( '.bulb-question' ).forEach( questionContainer => {
+	const questionId = questionContainer.id;
+	const questionData = window[ questionId ];
+	ReactDOM.render( <Question questionData={ questionData } />, questionContainer );
+} );
