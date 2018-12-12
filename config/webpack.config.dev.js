9095644bfec2a0196409eea3896df1bc11249ac4
Carlos E Silva <cedas@bu.edu>
2018-12-12 13:15:41 -0500

    Use published @carlosesilva/react-questions
    Whitelist node_modules under the @carlosesilva scope in the stylesheet rule

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -100,7 +100,7 @@ module.exports = {
 			},
 			{
 				test: /\.s?css$/,
-				exclude: /(node_modules|bower_components|editor\.s?css$)/,
+				exclude: /(node_modules(?!\/@carlosesilva)|bower_components|editor\.s?css$)/,
 				use: blocksCSSPlugin.extract( extractConfig ),
 			},
 			{
--- src/frontend.js
+++ src/frontend.js
@@ -6,8 +6,8 @@
 import ReactDOM from 'react-dom';
 
 // Import the react-questions library.
-import { Question } from 'react-questions';
-import 'react-questions/es/Question.css';
+import { Question } from '@carlosesilva/react-questions';
+import '@carlosesilva/react-questions/es/Question.css';
 
 // Find all .bulb-question DOM containers, collect their data from window and render <Questions> into them.
 document.querySelectorAll( '.bulb-question' ).forEach( questionContainer => {
