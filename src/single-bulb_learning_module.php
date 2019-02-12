d39baca80d1b0ec90c1a2206d3c15e39589dfca4
DannyCrews <dancrews@me.com>
2019-02-12 13:33:34 -0500

    CPT template enhancements.

--- src/single-bulb_learning_module.php
+++ src/single-bulb_learning_module.php
@@ -1,29 +1,115 @@
 <?php
 /**
- * Template file used to render a single post.
+ * Template file used to render a single learning module.
  *
- * @package Responsive_Framework
+ * @package BU Learning Blocks
+ * @since 0.0.2
  */
 
-get_header(); ?>
+get_header();
+?>
 
-	<?php while ( have_posts() ) : the_post(); ?>
+	<section id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<?php
 
-		<?php edit_post_link( __( 'Edit Learning Module', 'bulearningblocks' ), '<span class="edit-link">', '</span>' ); ?>
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+				?>
 
-		<article id="post-<?php the_ID(); ?>" <?php post_class( 'content-area' ); ?>>
-			<header>
-				<?php the_title(); ?>
-			</header>
+					<article id="post-<?php the_ID(); ?>" <?php post_class( 'content-area' ); ?>>
+						<header class="entry-header">
+							<?php
+							the_title( '<h1 class="entry-title">', '</h1>' );
+							?>
+								<div class="entry-meta">
+								<?php
+								// Edit post link.
+									edit_post_link(
+										sprintf(
+											wp_kses(
+												/* translators: %s: Name of current post. Only visible to screen readers. */
+												__( 'Edit <span class="screen-reader-text">%s</span>', 'bulearningblocks' ),
+												array(
+													'span' => array(
+														'class' => array(),
+													),
+												)
+											),
+											get_the_title()
+										),
+										'<span class="dashicons dashicons-edit"></span>'
+									);
+								?>
+							</div><!-- .meta-info -->
+						</header>
 
-			<?php the_content(); ?>
+						<div class="entry-content">
+							<?php
+							the_content(
+								sprintf(
+									wp_kses(
+										/* translators: %s: Name of current post. Only visible to screen readers */
+										__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'bulearningblocks' ),
+										array(
+											'span' => array(
+												'class' => array(),
+											),
+										)
+									),
+									get_the_title()
+								)
+							);
 
-			<?php edit_post_link( __( 'Edit Learning Module', 'bulearningblocks' ), '<span class="edit-link">', '</span>' ); ?>
+							wp_link_pages(
+								array(
+									'before' => sprintf( '<div class="page-link">%s', esc_html__( 'Module Pages:', 'bulearningblocks' ) ),
+									'after'  => '</div>',
+								)
+							);
+							?>
+						</div><!-- .entry-content -->
 
-		</article>
+						<footer class="entry-footer">
+							<?php
+							// Edit post link.
+							edit_post_link(
+								sprintf(
+									wp_kses(
+										/* translators: %s: Name of current post. Only visible to screen readers. */
+										__( 'Edit <span class="screen-reader-text">%s</span>', 'bulearningblocks' ),
+										array(
+											'span' => array(
+												'class' => array(),
+											),
+										)
+									),
+									get_the_title()
+								),
+								'<span class="dashicons dashicons-edit"></span>'
+							);
+							?>
+						</footer><!-- .entry-footer -->
+					</article><!-- #post-${ID} -->
 
-	<?php endwhile; ?>
+					<?php
+					the_post_navigation(
+						array(
+							'next_text' =>
+							'<span class="meta-nav" aria-hidden="true">' . __( 'Next Post', 'bulearningblocks' ) . '</span> ' . '<span class="screen-reader-text">' . __( 'Next post:', 'bulearningblocks' ) . '</span> <br/>' . '<span class="post-title">%title</span>',
 
-<?php get_sidebar( 'posts' ); ?>
+							'prev_text' =>
+							'<span class="meta-nav" aria-hidden="true">' . __( 'Previous Post', 'bulearningblocks' ) . '</span> ' . '<span class="screen-reader-text">' . __( 'Previous post:', 'bulearningblocks' ) . '</span> <br/>' . '<span class="post-title">%title</span>',
+						)
+					);
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</section><!-- #primary -->
+
 <?php
-	get_footer();
+get_sidebar( 'posts' );
+get_footer();
