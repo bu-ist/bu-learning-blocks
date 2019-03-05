a20f83b7748a200f50bd3bbfa1479e592c16c2ee
DannyCrews <dancrews@me.com>
2019-03-05 14:00:00 -0500

    Add module links and a bit of styling.

--- src/single-bulb_learning_module.php
+++ src/single-bulb_learning_module.php
@@ -1,115 +1,94 @@
 <?php
 /**
- * Template file used to render a single learning module.
+ * The template for displaying learning module pages.
  *
- * @package BU Learning Blocks
- * @since 0.0.2
+ * @package bu-learning-blocks
  */
 
 get_header();
 ?>
 
-	<section id="primary" class="content-area">
+	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-			<?php
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-				?>
-
-					<article id="post-<?php the_ID(); ?>" <?php post_class( 'content-area' ); ?>>
-						<header class="entry-header">
-							<?php
-							the_title( '<h1 class="entry-title">', '</h1>' );
-							?>
-								<div class="entry-meta">
-								<?php
-								// Edit post link.
-									edit_post_link(
-										sprintf(
-											wp_kses(
-												/* translators: %s: Name of current post. Only visible to screen readers. */
-												__( 'Edit <span class="screen-reader-text">%s</span>', 'bulearningblocks' ),
-												array(
-													'span' => array(
-														'class' => array(),
-													),
-												)
-											),
-											get_the_title()
-										),
-										'<span class="dashicons dashicons-edit"></span>'
-									);
-								?>
-							</div><!-- .meta-info -->
-						</header>
-
-						<div class="entry-content">
-							<?php
-							the_content(
-								sprintf(
-									wp_kses(
-										/* translators: %s: Name of current post. Only visible to screen readers */
-										__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'bulearningblocks' ),
-										array(
-											'span' => array(
-												'class' => array(),
-											),
-										)
-									),
-									get_the_title()
-								)
-							);
-
-							wp_link_pages(
-								array(
-									'before' => sprintf( '<div class="page-link">%s', esc_html__( 'Module Pages:', 'bulearningblocks' ) ),
-									'after'  => '</div>',
-								)
-							);
-							?>
-						</div><!-- .entry-content -->
-
-						<footer class="entry-footer">
-							<?php
-							// Edit post link.
-							edit_post_link(
-								sprintf(
-									wp_kses(
-										/* translators: %s: Name of current post. Only visible to screen readers. */
-										__( 'Edit <span class="screen-reader-text">%s</span>', 'bulearningblocks' ),
-										array(
-											'span' => array(
-												'class' => array(),
-											),
-										)
-									),
-									get_the_title()
-								),
-								'<span class="dashicons dashicons-edit"></span>'
-							);
-							?>
-						</footer><!-- .entry-footer -->
-					</article><!-- #post-${ID} -->
-
-					<?php
-					the_post_navigation(
-						array(
-							'next_text' =>
-							'<span class="meta-nav" aria-hidden="true">' . __( 'Next Post', 'bulearningblocks' ) . '</span> ' . '<span class="screen-reader-text">' . __( 'Next post:', 'bulearningblocks' ) . '</span> <br/>' . '<span class="post-title">%title</span>',
-
-							'prev_text' =>
-							'<span class="meta-nav" aria-hidden="true">' . __( 'Previous Post', 'bulearningblocks' ) . '</span> ' . '<span class="screen-reader-text">' . __( 'Previous post:', 'bulearningblocks' ) . '</span> <br/>' . '<span class="post-title">%title</span>',
-						)
-					);
-
-			endwhile; // End of the loop.
+		<?php
+		while ( have_posts() ) :
+			the_post();
 			?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="entry-header">
+						<?php
+							the_title( '<h1 class="entry-title">', '</h1>' );
+						?>
+					</header><!-- .entry-header -->
+					<div class="bulb-container bulb-container--narrow bulb-page-section">
+						<?php
+						$the_parent = wp_get_post_parent_id( get_the_id() );
+						$test_array = get_pages(
+							array(
+								'child_of' => get_the_ID(),
+								'post_type' => 'bulb_learning_module',
+							)
+						);
+						if ( $the_parent || $test_array ) { ?>
+						<div class="bulb-page-links">
+							<h3 class="bulb-page-links__title"><a href="<?php echo get_permalink( $the_parent ); ?>"><?php echo get_the_title( $the_parent ); ?></a></h3>
+							<ul class="bulb-min-list">
+								<?php
+								if ( $the_parent ) {
+									$find_children_of = $the_parent;
+								} else {
+									$find_children_of = get_the_ID();
+								}
+								wp_list_pages(
+									array(
+										'title_li'    => null,
+										'post_type'   => 'bulb_learning_module',
+										'child_of'    => $find_children_of,
+										'sort_column' => 'menu_order',
+									)
+								);
+								?>
+							</ul>
+						</div>
+						<?php } ?>
+					</div>
+					<div class="entry-content">
+						<?php
+						the_content(
+							sprintf(
+								wp_kses(
+									/* translators: %s: Name of current post. Only visible to screen readers */
+									__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', '_s' ),
+									array(
+										'span' => array(
+											'class' => array(),
+										),
+									)
+								),
+								get_the_title()
+							)
+						);
+						wp_link_pages(
+							array(
+								'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
+								'after'  => '</div>',
+							)
+						);
+						?>
+					</div><!-- .entry-content -->
+
+					<footer class="entry-footer">
+					</footer><!-- .entry-footer -->
+				</article><!-- #post-<?php the_ID(); ?> -->
+			<?php
+			the_post_navigation();
+		endwhile; // End of the loop.
+		?>
 
 		</main><!-- #main -->
-	</section><!-- #primary -->
+	</div><!-- #primary -->
 
 <?php
-get_sidebar( 'posts' );
+get_sidebar();
 get_footer();
