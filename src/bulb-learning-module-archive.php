b662eb5506a785e947ec0c3bb013d6a1e6dba9d6
DannyCrews <dancrews@me.com>
2019-11-20 13:44:22 -0500

    Add course titles to cpt archive.

--- src/bulb-learning-module-archive.php
+++ src/bulb-learning-module-archive.php
@@ -9,48 +9,45 @@
 
 get_header();
 
-$args = array(
-	'paged'          => $paged,
-	'post_parent'    => 0,
-	'post_type'      => 'bulb-learning-module',
-	'orderby'        => 'title',
-	'order'          => 'ASC',
-	'posts_per_page' => -1,
-);
+$terms = get_terms(
+	'bulb-courses',
+	array(
+		'orderby'    => 'name',
+		'order'      => 'ASC',
+		'hide_empty' => 1,
+	)
+); ?>
+<h1>Course Index:</h1>
 
-$bulb_query = new WP_Query( $args );
-
-/**
- * Enqueue the custom post type's single- template.
- *
- * @param string $bulb_query query to be filtered.
- *
- * @return string $bulb_query Filtered query.
- *
- * @since 0.0.2
- */
-function get_bulb_parents( $bulb_query ) {
-	if ( is_archive() && false === $bulb_query->query_vars['post_parent'] && 'bulb-learning-module' === $bulb_query->query_vars['post_type'] ) {
-		$bulb_query->set( 'post_parent', 0 );
-	}
-	return $bulb_query;
-}
-add_filter( 'pre_get_posts', 'get_bulb_parents' );
-?>
-
-<h1>Lesson Parent Pages:</h1>
 <div class="bulb-container bulb-container--narrow bulb-page-section">
 <?php
-while ( $bulb_query->have_posts() ) {
-	$bulb_query->the_post();
-	?>
-<div class="post-item">
-	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-	<?php
+
+foreach( $terms as $term ) {
+	// Define the query
+	$args = array(
+		'paged'          => $paged,
+		'posts_per_page' => -1,
+		'post_type' => 'bulb-learning-module',
+		'orderby'        => 'title',
+		'order'          => 'ASC',
+		'bulb-courses' => $term->slug
+	);
+	$query = new WP_Query( $args );
+
+	echo'<h2>' . $term->name . '</h2>';
+
+	echo '<ul>';
+	while ( $query->have_posts() ) : $query->the_post(); ?>
+	<li class="post-item" id="post-<?php the_ID(); ?>">
+		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+	</li>
+		<?php
+	endwhile;
+
+	echo '</ul>';
+	wp_reset_postdata();
 }
 ?>
-
 </div>
-
 <?php
 get_footer();
