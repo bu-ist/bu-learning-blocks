9ba6e2188dd3cbb5ab99dcc3bd2c2a0faa27af1d
DannyCrews <dancrews@me.com>
2019-12-16 14:08:51 -0500

    Refine uninstall.php.

--- uninstall.php
+++ uninstall.php
@@ -3,31 +3,32 @@
  * Uninstalls plugin options and custom post types.
  *
  * @package BU Learning Blocks
+ *
+ * @since Version 0.0.6
  **/
 
+/* Exit if plugin delete hasn't been called */
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-if ( ! get_option( 'plugin_do_uninstall', false ) ) {
-	exit;
+$options = array(
+	'bulb_active',
+	'bulb_cpt_install',
+);
+foreach ( $options as $option ) {
+	if ( get_option( $option ) ) {
+		delete_option( $option );
+	}
 }
 
-// $options = array(
-// 	'bulb_active',
-// 	'bulb_cpt_install',
-// );
-// foreach ( $options as $option ) {
-// 	if ( get_option( $option ) ) {
-// 		delete_option( $option );
-// 	}
-// }
-
-// $bulb_cpt_args  = array(
-// 	'post_type'      => 'bulb-learning-module',
-// 	'posts_per_page' => -1,
-// );
-// $bulb_cpt_posts = get_posts( $bulb_cpt_args );
-// foreach ( $bulb_cpt_posts as $bulb_post ) {
-// 	wp_delete_post( $bulb_post->ID, false );
-// }
+$bulb_cpt_args  = array(
+	'post_type'      => 'bulb-learning-module',
+	'posts_per_page' => -1,
+);
+$bulb_cpt_posts = get_posts( $bulb_cpt_args );
+if ( ! empty( $bulb_cpt_posts ) ) {
+	foreach ( $bulb_cpt_posts as $bulb_post ) {
+		wp_delete_post( $bulb_post->ID, false );
+	}
+}
