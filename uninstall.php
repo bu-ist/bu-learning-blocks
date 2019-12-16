6a1dbc0e2f9d52efdfdeb013048d241e356b0395
DannyCrews <dancrews@me.com>
2019-12-16 09:30:17 -0500

    Add deletion of custom post types to uninstall.php.

--- uninstall.php
+++ uninstall.php
@@ -1,7 +1,33 @@
-<?php // exit if uninstall constant is not defined
-if (!defined('WP_UNINSTALL_PLUGIN')) exit;
+<?php
+/**
+ * Uninstalls plugin options and custom post types.
+ *
+ * @package BU Learning Blocks
+ **/
 
-delete_option( 'bulb_active' );
-delete_option( 'bulb_cpt_install' );
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
 
-if (!get_option('plugin_do_uninstall', false)) exit;
+$options = array(
+	'bulb_active',
+	'bulb_cpt_install',
+);
+foreach ( $options as $option ) {
+	if ( get_option( $option ) ) {
+		delete_option( $option );
+	}
+}
+
+$bulb_cpt_args  = array(
+	'post_type'      => 'bulb_learning_module',
+	'posts_per_page' => -1,
+);
+$bulb_cpt_posts = get_posts( $bulb_cpt_args );
+foreach ( $bulb_cpt_posts as $bulb_post ) {
+	wp_delete_post( $bulb_post->ID, false );
+}
+
+if ( ! get_option( 'plugin_do_uninstall', false ) ) {
+	exit;
+}
