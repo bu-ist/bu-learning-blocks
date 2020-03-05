c7e6838ad1dc75bc315f39d16f83b5370dfd7b05
DannyCrews <dancrews@me.com>
2020-03-05 10:25:51 -0500

    Update release version.

--- bu-learning-blocks.php
+++ bu-learning-blocks.php
@@ -7,7 +7,7 @@
  * Author URI: http://www.bu.edu/
  * Text Domain: bulearningblocks
  * Domain Path: /languages
- * Version: 0.0.6
+ * Version: v0.0.7
  * License: GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  *
