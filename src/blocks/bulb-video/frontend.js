2e45c7a5733b5d705e996c99b6d121fbfc22d532
Unknown <tamlin@mac.com>
2021-05-05 10:41:02 -0400

    :fire: remove moved code

--- src/blocks/bulb-video/frontend.js
+++ src/blocks/bulb-video/frontend.js
@@ -5,33 +5,12 @@ import Captions from './frontend/captions';
 
 // Starter code from https://stackoverflow.com/questions/32142656/get-youtube-captions/58435817#58435817
 
-function convertMS( milliseconds ) {
-	var day, hour, minute, seconds;
-	seconds = Math.floor(milliseconds / 1000);
-	minute = Math.floor(seconds / 60);
-	seconds = seconds % 60;
-	hour = Math.floor(minute / 60);
-	minute = minute % 60;
-	day = Math.floor(hour / 24);
-	hour = hour % 24;
-
-	// Add leading zeroes
-	hour = ( hour < 10 ) ? "0" + hour : hour;
-	minute = ( minute < 10 ) ? "0" + minute : minute;
-	seconds = ( seconds < 10 ) ? "0" + seconds : seconds;
-
-	return `${hour}:${minute}:${seconds}`;
-}
-
 function seekToTime( event ) {
 	if ( event.target.className === 'bulb-video-caption' ) {
 		const time = event.target.firstElementChild.dataset.seekTo;
 		player.seekTo( time );
 	}
 }
-
-
-
 
 const VideoWithCaptions = ( { videoID } ) => {
 	return (
