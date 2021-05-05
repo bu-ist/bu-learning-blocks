866234c2eb6ae3683f762d4a2cf323c3f53dd26a
Unknown <tamlin@mac.com>
2021-05-04 23:34:01 -0400

    add caption component

--- src/blocks/bulb-video/frontend.js
+++ src/blocks/bulb-video/frontend.js
@@ -5,14 +5,6 @@ import Captions from './frontend/captions';
 
 // Starter code from https://stackoverflow.com/questions/32142656/get-youtube-captions/58435817#58435817
 
-const youtubeID = 'bNTSO3D5bc8';
-let player;
-
-const captionsContainer = document.createElement( 'div' );
-const videoBlock = document.querySelector( '.bulb-video' );
-
-captionsContainer.classList.add( 'bulb-video-captions-container' );
-
 function convertMS( milliseconds ) {
 	var day, hour, minute, seconds;
 	seconds = Math.floor(milliseconds / 1000);
@@ -38,43 +30,7 @@ function seekToTime( event ) {
 	}
 }
 
-// Get the transcript. Requires a named transcript.
-// This should probably be switched to
-// https://developers.google.com/youtube/v3/docs/captions/download
-// but that involves an API key, and I'm also not sure how to deal
-// with some of the web formats it returns.
-//
-// That would be nice to figure out though, because one thing I can't
-// get to from here is chapters.
-//
-// Chapters are added in brackets to the video description,
-// and you get to them using the API here:
-// https://developers.google.com/youtube/v3/docs/videos/list
-//
-// A really nice enhancement would be to collapse the transcript by chapter, if it's available.
-fetch('https://www.youtube.com/api/timedtext?v=' + youtubeID +'&lang=en-US&fmt=json3')
-	.then(response => response.json())
-	.then(data => {
-			// Get the captions
-			const captions = data.events;
-			captionsContainer.addEventListener('click', seekToTime, true);
 
-			// For each caption, create a caption div and timestamp
-			for ( const caption of captions ) {
-				const timestamp = caption[ 'tStartMs' ];
-				const timeInSeconds = Math.floor( timestamp / 1000 );
-				const captionText = caption[ 'segs' ][ 0 ].utf8;
-
-				// The pointer events settings below ensure the click event always
-				// uses the bulb-video-caption as the target.
-				captionsContainer.insertAdjacentHTML( 'beforeend',
-					`<div class="bulb-video-caption">
-						<time class="bulb-video-caption-timestamp" datetime="P${timeInSeconds}S" data-seek-to="${timeInSeconds}" style="pointer-events: none;">${convertMS(timestamp)}</time>
-						<p style="pointer-events: none;">${captionText}</p>
-					</div>`
-				);
-			}
-});
 
 
 const VideoWithCaptions = ( { videoID } ) => {
--- src/blocks/bulb-video/frontend/captions.js
+++ src/blocks/bulb-video/frontend/captions.js
@@ -1,7 +1,52 @@
+import axios from 'axios';
+import { useState, useEffect } from '@wordpress/element';
+
 export default function Captions( { videoID } ) {
+	const [ captions, setCaptions ] = useState( [] );
+
+	useEffect( () => {
+		initData();
+	}, [] );
+
+	const initData = async () => {
+		const { data: { events: captions } } = await axios.get(
+			`https://www.youtube.com/api/timedtext?v=${ videoID }&lang=en-US&fmt=json3`
+		);
+		setCaptions( captions );
+	};
+
+	const convertMS = ( milliseconds ) => {
+		const seconds = Math.floor( milliseconds / 1000 );
+		const minutes = Math.floor( seconds / 60 );
+		const hours = Math.floor( minutes / 60 );
+
+		const counterSecs = seconds % 60;
+		const counterMins = minutes % 60;
+
+		const displaySecs = counterSecs < 10 ? `0${counterSecs}` : counterSecs;
+		const displayMins = counterMins < 10 ? `0${counterMins}` : counterMins;
+
+		return `${ hours }:${ displayMins }:${ displaySecs }`;
+	};
+
 	return (
 		<div className="bulb-video-captions-container">
-			{videoID}
+			{ captions &&
+				captions.map( ( caption ) => {
+					const seconds = Math.floor( caption[ 'tStartMs' ] );
+
+					return (
+						<div key={caption['tStartMs']} className="bulb-video-caption">
+						<time
+							className="bulb-video-caption-timestamp"
+							dateTime={ `P${ seconds }S` }
+						>
+								{ convertMS( caption.tStartMs ) }
+						</time>
+							<p>{ caption[ 'segs' ][ 0 ].utf8 }</p>
+					</div>
+					);
+				} ) }
 		</div>
 	);
 }
