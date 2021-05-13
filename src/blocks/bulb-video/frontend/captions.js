601af78fd2e12ee5ba8b6797529cca201ac85b07
Unknown <tamlin@mac.com>
2021-05-13 10:55:41 -0400

    spacing fixes

--- src/blocks/bulb-video/frontend/captions.js
+++ src/blocks/bulb-video/frontend/captions.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 import { useState, useEffect } from '@wordpress/element';
 
-import CaptionItem from "./captionItem";
+import CaptionItem from './captionItem';
 
-export default function Captions( { videoID, playerRef } ) {
-	const [ captions, setCaptions ] = useState( [] );
+export default function Captions({ videoID, playerRef }) {
+	const [captions, setCaptions] = useState([]);
 
-	const [ searchString, setSearchString ] = useState( '' );
+	const [searchString, setSearchString] = useState('');
 
-	useEffect( () => {
+	useEffect(() => {
 		initData();
-	}, [] );
+	}, []);
 
 	const initData = async () => {
 		// Fetch caption track listing first.
 		const { data } = await axios.get(
-			`https://www.youtube.com/api/timedtext?v=${ videoID }&type=list`
+			`https://www.youtube.com/api/timedtext?v=${videoID}&type=list`
 		);
 
 		// The track listing response is XML, so parse it as a DOM Document.
@@ -27,31 +27,41 @@ export default function Captions( { videoID, playerRef } ) {
 		// We're just fetching the first track, assuming there aren't multiple languages.
 		// Ultimately, there should be a UI to switch between any available caption tracks.
 		const firstTrackLangCode = captionsList
-			.getElementsByTagName( 'track' )[ 0 ]
-			.getAttribute( 'lang_code' );
+			.getElementsByTagName('track')[0]
+			.getAttribute('lang_code');
 
 		// Fetch the actual captions.
 		const {
 			data: { events },
 		} = await axios.get(
-			`https://www.youtube.com/api/timedtext?v=${ videoID }&lang=${ firstTrackLangCode }&fmt=json3`
+			`https://www.youtube.com/api/timedtext?v=${videoID}&lang=${firstTrackLangCode}&fmt=json3`
 		);
-		setCaptions( events );
+		setCaptions(events);
 	};
 
-	const filteredCaptions = captions.filter( ({ segs }) => (
-		segs[0].utf8.toLowerCase().indexOf( searchString.toLowerCase() ) !== -1
-	) );
+	// Filter captions by search string, returns full set if search string is empty.
+	const filteredCaptions = captions.filter(
+		({ segs }) =>
+			segs[0].utf8.toLowerCase().indexOf(searchString.toLowerCase()) !==
+			-1
+	);
 
 	return (
 		<div className="bulb-video-captions-container">
 			<input
 				type="text"
-				onChange={ ({ target: { value } }) => setSearchString(value) }
+				onChange={({ target: { value } }) => setSearchString(value)}
 				value={searchString}
 			/>
-			{ captions &&
-				filteredCaptions.map((caption) => <CaptionItem caption={caption} searchString={searchString} playerRef={playerRef} /> ) }
+			{captions &&
+				filteredCaptions.map((caption) => (
+					<CaptionItem
+						key={caption.tStartMs}
+						caption={caption}
+						searchString={searchString}
+						playerRef={playerRef}
+					/>
+				))}
 		</div>
 	);
 }
